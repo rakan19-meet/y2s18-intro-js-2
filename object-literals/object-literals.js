552f10ada064eae8fe3717a336c39725743e6a85
rakan19-meet <rakan19@meet.mit.edu>
2018-08-01 07:14:48 -0400

    trump and kim jong un

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,9 @@
-// Create your 'me' object literal here!
+var rakan = {
+	firstname : "Rakan",
+	lastname : "Darwish",
+	favoritefood : ["sea food", "wara2 dawali"],
+	age	: 17,
+	picture : " https://sharing.wcpo.com/sharescnn/photo/2018/06/10/S094755020_1528680028645_89452883_ver1.0_640_480.JPG",
+}
+rakan = JSON.stringify(rakan)
+document.write(rakan)
